45c11e4d0013fa350b497519b81b8abec91f4433
zakaria elalaoui <zakarialaoui10@gmail.com>
2026-04-10 21:10:47 +0100

    rm build

--- build/wiscus/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '2448a4ee11890b2d4001');
--- build/wiscus/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'fc1f99a6a414fe62ccf0');
